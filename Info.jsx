c4fe305f0c82be3c4eabfdb95c23b2e7397847c0
TPratiba <86460267+TPratiba@users.noreply.github.com>
2021-06-25 11:04:36 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Info() {
+
+return (
+  <div className = "note">
+<h1 >Javascript and react.js</h1>
+<p >A basic web dev react Js Bootcamp</p>
+</div>
+);
+
+}
+
+export default Info;
